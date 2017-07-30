8106bf22106a66cf60334de3d206936f96612249
Brandon Semilla <semibran@gmail.com>
2017-07-30 12:14:34 -0700

    move raf to bottom of loop

--- test.js
+++ test.js
@@ -8,6 +8,6 @@ const keys = require('./')(window, {
 loop()
 
 function loop() {
-	requestAnimationFrame(loop)
 	document.body.innerHTML = JSON.stringify(keys, null, 2)
+  requestAnimationFrame(loop)
 }
